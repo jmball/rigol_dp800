b1cbf595fae726d22c571bf9549fb8793e246360
Greyson Christoforo <grey@christoforo.net>
2020-11-23 18:06:53 +0000

    PEP517 compliant packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="rigol_dp800",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    author="James Ball",
-    author_email="",
-    description="Rigol DP800 series control library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jmball/rigol_dp800",
-    py_modules=["dp800", "virtual_dp800"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
