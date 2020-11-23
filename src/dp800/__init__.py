e3ffbb2d7a6e809f29e80a2b32f03aaa8f468b30
Greyson Christoforo <grey@christoforo.net>
2020-11-23 18:08:51 +0000

    fix __version__

--- src/dp800/__init__.py
+++ src/dp800/__init__.py
@@ -1,18 +1,9 @@
-from setuptools_scm import get_version
 from pkg_resources import get_distribution, DistributionNotFound
 
-# get version if running from git folder
-try:
-    __version__ = get_version(root="..", relative_to=__file__)
-except LookupError:
-    __version__ = None
-
 # get version if package has been installed
-if __version__ is None:
-    try:
-        __version__ = get_distribution(__name__).version
-    except DistributionNotFound:
-        __version__ = "0.0.0"
-        pass
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "0.0.0"
 
 from .dp800 import *
